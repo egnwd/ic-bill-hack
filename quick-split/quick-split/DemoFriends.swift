e7f1ae0c289193e0270a661e4104b108fb24ea2d
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2016-02-20 17:12:09 +0000

    [EG][MR] Update file names

--- quick-split/quick-split/DemoFriends.swift
+++ quick-split/quick-split/DemoFriends.swift
@@ -14,16 +14,16 @@ class DemoFriends {
   static let sharedInstance = DemoFriends()
   
   init() {
-    let alice = Friend(name: "Alice", pictureName: "alice.png", mondoId: "acc_1")
-    let bob = Friend(name: "Bob", pictureName: "bob.png", mondoId: "acc_2")
-    let clare = Friend(name: "Clare", pictureName: "clare.png", mondoId: "acc_3")
-    let david = Friend(name: "David", pictureName: "david.png", mondoId: "acc_4")
-    let elliot = Friend(name: "Elliot", pictureName: "elliot.png", mondoId: "acc_5")
-    let fred = Friend(name: "Fred", pictureName: "fred.png", mondoId: "acc_6")
-    let george = Friend(name: "George", pictureName: "george.png", mondoId: "acc_7")
-    let harry = Friend(name: "Harry", pictureName: "harry.png", mondoId: "acc_8")
-    let isaac = Friend(name: "Isaac", pictureName: "isaac.png", mondoId: "acc_9")
-    let jonathan = Friend(name: "Jonathan", pictureName: "jonathan.png", mondoId: "acc_10")
+    let alice = Friend(name: "Alice", pictureName: "alice.jpg", mondoId: "acc_1")
+    let bob = Friend(name: "Bob", pictureName: "bob.jpg", mondoId: "acc_2")
+    let clare = Friend(name: "Clare", pictureName: "clare.jpg", mondoId: "acc_3")
+    let david = Friend(name: "David", pictureName: "david.jpg", mondoId: "acc_4")
+    let elliot = Friend(name: "Elliot", pictureName: "elliot.jpg", mondoId: "acc_5")
+    let fred = Friend(name: "Fred", pictureName: "fred.jpg", mondoId: "acc_6")
+    let george = Friend(name: "George", pictureName: "george.jpg", mondoId: "acc_7")
+    let harry = Friend(name: "Harry", pictureName: "harry.jpg", mondoId: "acc_8")
+    let isaac = Friend(name: "Isaac", pictureName: "isaac.jpg", mondoId: "acc_9")
+    let jonathan = Friend(name: "Jonathan", pictureName: "jonathan.jpg", mondoId: "acc_10")
     
     friends = [alice, bob, clare, david, elliot, fred, george, harry, isaac, jonathan]
   }
