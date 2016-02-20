924bae9b7ffc2efcb35e66c982a63577692c6786
Jonathan King <jonathanlking@me.com>
2016-02-20 22:52:43 +0000

    Not working - will switch back to earlier verison

--- imagepartition/imagepartition/SelectImageView.m
+++ imagepartition/imagepartition/SelectImageView.m
@@ -31,6 +31,45 @@
     return self;
 }
 
+
+
+//- (UIImage *)processImage:(UIImage*)input {
+//    
+//    
+//    
+//    
+//    for (int i = start; i <= end; i++) {
+//        NSLog(@"%d", emptyRow[i]);
+//    }
+//    
+//    for (NSUInteger j = 0; j < inputHeight; j++) {
+//        
+//        if (j < start || j > end) {
+//            for (NSUInteger i = 0; i < inputWidth; i++) {
+//                UInt32 * currentPixel = inputPixels + (j * inputWidth) + i;
+//                *currentPixel = RGBAMake(255, 255, 255, 255);
+//            }
+//        }
+//    }
+//    
+//    
+//    NSLog(@"Max of length: %d, between (%d, %d)", end - start, start, end);
+//    
+//    // 4. Create a new UIImage
+//    CGImageRef newCGImage = CGBitmapContextCreateImage(context);
+//    UIImage * processedImage = [UIImage imageWithCGImage:newCGImage];
+//    
+//    //    for (int i = 0; i < inputHeight; i++) {
+//    //        NSLog(@"%d", emptyRow[i]);
+//    //    }
+//    
+//    // 5. Cleanup!
+//
+//    
+//    return processedImage;
+//}
+
+
 - (void)updateBoundingRect {
     
     // There must be at least one point
@@ -78,6 +117,103 @@
                             minY,
                             maxX - minX,
                             maxY - minY);
+    
+    // Now we have the user selected bound, perform image processing!
+    
+    
+    // Borrowed with love from
+    // http://www.raywenderlich.com/69855/image-processing-in-ios-part-1-raw-bitmap-modification
+    
+    #define Mask8(x) ( (x) & 0xFF )
+    #define R(x) ( Mask8(x) )
+    #define G(x) ( Mask8(x >> 8 ) )
+    #define B(x) ( Mask8(x >> 16) )
+    #define A(x) ( Mask8(x >> 24) )
+    #define RGBAMake(r, g, b, a) ( Mask8(r) | Mask8(g) << 8 | Mask8(b) << 16 | Mask8(a) << 24 )
+    
+    // 1. Get the raw pixels of the image
+    UInt32 * inputPixels;
+    
+    CGImageRef inputCGImage = [self.image CGImage];
+    NSUInteger inputWidth = CGImageGetWidth(inputCGImage);
+    NSUInteger inputHeight = CGImageGetHeight(inputCGImage);
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    NSUInteger bytesPerPixel = 4;
+    NSUInteger bitsPerComponent = 8;
+    
+    NSUInteger inputBytesPerRow = bytesPerPixel * inputWidth;
+    
+    inputPixels = (UInt32 *)calloc(inputHeight * inputWidth, sizeof(UInt32));
+    
+    CGContextRef context = CGBitmapContextCreate(inputPixels, inputWidth, inputHeight,
+                                                 bitsPerComponent, inputBytesPerRow, colorSpace,
+                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, inputWidth, inputHeight), inputCGImage);
+    
+    boolean_t *emptyRow = (boolean_t *)calloc(inputHeight, sizeof(boolean_t));
+    
+    // 3. Convert the image to Black & White
+    for (NSUInteger j = 0; j < inputHeight; j++) {
+        emptyRow[j] = 0;
+        for (NSUInteger i = 0; i < inputWidth; i++) {
+            UInt32 * currentPixel = inputPixels + (j * inputWidth) + i;
+            UInt32 color = *currentPixel;
+            
+            // Average of RGB = greyscale
+            UInt32 averageColor = (R(color) + G(color) + B(color)) / 3.0;
+            averageColor = (averageColor > 45) ? 255 : 0;
+            
+            emptyRow[j] |= (255 - averageColor);
+            
+            *currentPixel = RGBAMake(averageColor, averageColor, averageColor, A(color));
+        }
+    }
+    
+    // 4. Find longest chain where there are no more than 5 consective empty rows
+    
+    #define MAX_ZERO 5
+    
+    // The start and end of the maximum sequence
+    int start = 0;
+    int end = 0;
+    
+    int count = 0; // number of consecutive empty rows encountered, initially zero
+    int new_start = 0;
+    
+    for (int j = 0; j < inputHeight; j++) {
+        if (emptyRow[j] == 0) {
+            if (count == 0) {
+                // We have reached the end of the sequence, or we have yet to begin a sequence
+                new_start = j+1;
+            } else {
+                count --;
+            }
+        } else {
+            // We have a one
+            if (j - new_start > end - start) {
+                // if we have a new max sequence, set it
+                start = new_start;
+                end = j;
+            }
+            // Reset the count to MAX_ZERO
+            count = MAX_ZERO;
+        }
+    }
+    
+    NSLog(@"Max of length: %d, between (%d, %d)", end - start, start, end);
+
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(context);
+    free(inputPixels);
+    free(emptyRow);
+    
+    selection = CGRectMake(selection.origin.x,
+                           selection.origin.y + start,
+                           selection.size.width,
+                           end - start);
     
     id<SelectImageViewDelegate> strongDelegate = self.delegate;
     
