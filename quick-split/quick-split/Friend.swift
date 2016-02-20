de422db5933cd8da42749a5cc838638b32a5ab2b
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2016-02-20 17:13:35 +0000

    [EG][MR] Make avatar a circle

--- quick-split/quick-split/Friend.swift
+++ quick-split/quick-split/Friend.swift
@@ -17,7 +17,24 @@ class Friend {
   
   init(name: String, pictureName: String, mondoId: String) {
     self.name = name
-    self.picture = UIImage(named: pictureName)!
+    self.picture = UIImage(named: pictureName)!.circle!
     self.mondoId = mondoId
+  }
+}
+
+private extension UIImage {
+  var circle: UIImage? {
+    let square = CGSize(width: min(size.width, size.height), height: min(size.width, size.height))
+    let imageView = UIImageView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: square))
+    imageView.contentMode = .ScaleAspectFill
+    imageView.image = self
+    imageView.layer.cornerRadius = square.width/2
+    imageView.layer.masksToBounds = true
+    UIGraphicsBeginImageContext(imageView.bounds.size)
+    guard let context = UIGraphicsGetCurrentContext() else { return nil }
+    imageView.layer.renderInContext(context)
+    let result = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return result
   }
 }
