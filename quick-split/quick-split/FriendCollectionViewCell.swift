a402b1c192c69a4e1120b85988a20ea687556cac
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2016-02-20 17:13:17 +0000

    [EG][MR] Correctly format `CollectionViewCell`

--- quick-split/quick-split/FriendCollectionViewCell.swift
+++ quick-split/quick-split/FriendCollectionViewCell.swift
@@ -11,27 +11,27 @@ import UIKit
 class FriendCollectionViewCell: UICollectionViewCell {
   let cellSize = CGSize(width: 75, height: 82)
 
-  var name: UILabel
-  var avatar: UIImageView
-  var friend: Friend
+  var name: UILabel = UILabel()
+  var avatar: UIImageView = UIImageView()
+  var friend: Friend?
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
   
-  init(friend: Friend, origin: CGPoint) {
-    let frame = CGRect(x: origin.x, y: origin.y, width: cellSize.width, height: cellSize.height)
+  func populateWithFriend(friend: Friend) {
     self.friend = friend
     
     let imageLength = 48
-    let imageX = Int(cellSize.width) - (imageLength/2)
+    let imageX = (Int(cellSize.width) - imageLength) / 2
     avatar = UIImageView(frame: CGRect(x: imageX, y: 0, width: imageLength, height: imageLength))
     avatar.image = friend.picture
+    self.addSubview(avatar)
     
     name = UILabel(frame: CGRect(x: 0, y: imageLength+10, width: Int(cellSize.width), height: 24))
     name.text = friend.name
-    
-    super.init(frame: frame)
+    name.textAlignment = .Center
+    self.addSubview(name)
   }
-
-  // Not called as we are not use a nib
-  required init?(coder aDecoder: NSCoder) {
-      fatalError("init(coder:) has not been implemented")
-  }
+  
 }
