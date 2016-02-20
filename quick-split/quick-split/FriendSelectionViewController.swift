50b8ce2a2cefd79c608c96001aaaaeec7c429f91
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2016-02-20 17:12:32 +0000

    [EG][MR] Populate `friendCollectionView`

--- quick-split/quick-split/FriendSelectionViewController.swift
+++ quick-split/quick-split/FriendSelectionViewController.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 
-class FriendSelectionViewController: UIViewController, UICollectionViewDelegate {
+class FriendSelectionViewController: UIViewController, UICollectionViewDataSource {
   
   var friends: [Friend] = []
-  @IBOutlet var friendCollectionView: UICollectionViewController!
-  
+  @IBOutlet var friendCollectionView: UICollectionView!
+  var colours: [UIColor] = []
   
   override func viewDidLoad() {
     super.viewDidLoad()
     friends = DemoFriends.getFriends()
-    friendCollectionView.delete(self)
+    let green = UIColor.greenColor()
+    let yellow = UIColor.yellowColor()
+    let purple = UIColor.purpleColor()
+    let red = UIColor.redColor()
+    let blue = UIColor.blueColor()
+    colours = [green, yellow, purple, red, blue]
       
     // Do any additional setup after loading the view.
   }
@@ -39,5 +44,14 @@ class FriendSelectionViewController: UIViewController, UICollectionViewDelegate 
   */
   
   // MARK: - Collection
-
+  
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return friends.count
+  }
+  
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = friendCollectionView.dequeueReusableCellWithReuseIdentifier("FriendCell", forIndexPath: indexPath) as! FriendCollectionViewCell
+    cell.populateWithFriend(friends[indexPath.row])
+    return cell
+  }
 }
