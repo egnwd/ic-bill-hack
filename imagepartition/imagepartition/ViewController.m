31bf28d1772c01b59bff0b05bb51074f9697d7b0
Jonathan King <jonathanlking@me.com>
2016-02-20 20:45:17 +0000

    Implement basic image processing

--- imagepartition/imagepartition/ViewController.m
+++ imagepartition/imagepartition/ViewController.m
@@ -57,6 +57,66 @@
     // Dispose of any resources that can be recreated.
 }
 
+// Borrowed with love from
+// http://www.raywenderlich.com/69855/image-processing-in-ios-part-1-raw-bitmap-modification
+
+#define Mask8(x) ( (x) & 0xFF )
+#define R(x) ( Mask8(x) )
+#define G(x) ( Mask8(x >> 8 ) )
+#define B(x) ( Mask8(x >> 16) )
+#define A(x) ( Mask8(x >> 24) )
+#define RGBAMake(r, g, b, a) ( Mask8(r) | Mask8(g) << 8 | Mask8(b) << 16 | Mask8(a) << 24 )
+
+- (UIImage *)processImageTwoTone:(UIImage*)input {
+
+    // 1. Get the raw pixels of the image
+    UInt32 * inputPixels;
+    
+    CGImageRef inputCGImage = [input CGImage];
+    NSUInteger inputWidth = CGImageGetWidth(inputCGImage);
+    NSUInteger inputHeight = CGImageGetHeight(inputCGImage);
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    NSUInteger bytesPerPixel = 4;
+    NSUInteger bitsPerComponent = 8;
+    
+    NSUInteger inputBytesPerRow = bytesPerPixel * inputWidth;
+    
+    inputPixels = (UInt32 *)calloc(inputHeight * inputWidth, sizeof(UInt32));
+    
+    CGContextRef context = CGBitmapContextCreate(inputPixels, inputWidth, inputHeight,
+                                                 bitsPerComponent, inputBytesPerRow, colorSpace,
+                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, inputWidth, inputHeight), inputCGImage);
+    
+    // 3. Convert the image to Black & White
+    for (NSUInteger j = 0; j < inputHeight; j++) {
+        for (NSUInteger i = 0; i < inputWidth; i++) {
+            UInt32 * currentPixel = inputPixels + (j * inputWidth) + i;
+            UInt32 color = *currentPixel;
+            
+            // Average of RGB = greyscale
+            UInt32 averageColor = (R(color) + G(color) + B(color)) / 3.0;
+            averageColor = (averageColor > 40) ? 255 : 0;
+            
+            *currentPixel = RGBAMake(averageColor, averageColor, averageColor, A(color));
+        }
+    }
+    
+    // 4. Create a new UIImage
+    CGImageRef newCGImage = CGBitmapContextCreateImage(context);
+    UIImage * processedImage = [UIImage imageWithCGImage:newCGImage];
+    
+    // 5. Cleanup!
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(context);
+    free(inputPixels);
+    
+    return processedImage;
+}
+
 /**
  * Method allocates and initializes the Scanning coordinator object.
  * Coordinator is initialized with settings for scanning
@@ -139,6 +199,8 @@
     [self.coordinator processImage:partitionImage
                     scanningRegion:CGRectMake(0.0, 0.0, 1.0, 1.0)
                           delegate:self];
+    
+    _partitionImageView.image = [self processImageTwoTone:partitionImage];
 
 }
 
