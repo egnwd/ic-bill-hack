00bf35374cc200f1810860c0c18a47d57a217e33
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2016-02-20 14:13:59 +0000

    [EG][MR] Create `Friend` object

---
+++ quick-split/quick-split/Friend.swift
@@ -0,0 +1,22 @@
+//
+//  Friend.swift
+//  quick-split
+//
+//  Created by Elliot Greenwood on 02.20.2016.
+//  Copyright © 2016 stealth-phoenix. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Friend {
+  var name: String 
+  var picture: UIImage
+  var mondoId: String
+  
+  init(name: String, pictureName: String, mondoId: String) {
+    self.name = name
+    self.picture = UIImage.init(named: pictureName)!
+    self.mondoId = mondoId
+  }
+}
