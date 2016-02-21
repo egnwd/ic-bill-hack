ebb96ddd8bb68a4b433eb893f0660f7c9cd00b71
Michael Radigan <michael.radigan14@imperial.ac.uk>
2016-02-21 01:54:17 +0000

    Create initial web app

---
+++ dinner-with/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DinnerWith::Application.config.secret_token = 'cd04c6f68daf20cea3901aa48f8ce8a9a0ab099ccfc93ed98b1fc6258ae780698d6b039a8b21a66ab5591688b88034f8a89da278f75510812c4bffc9e446a20b'
