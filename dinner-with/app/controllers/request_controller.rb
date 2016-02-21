f5385b8e985b512b0024203bb24b3e7b6ca5387c
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2016-02-21 04:54:11 +0000

    [EG][MR] Web app front end html

--- dinner-with/app/controllers/request_controller.rb
+++ dinner-with/app/controllers/request_controller.rb
@@ -1,6 +1,11 @@
 class RequestController < ApplicationController
   def create
-    render :text => "Create transaction_id: #{params[:transaction_id]}"
+    @request = Request.new(params[:request])
+    if @request.save
+      render :text => "Success: #{@request.inspect}",  :status => 200
+    else
+      render :text => "Error: Couldn't create transaction.",  :status => 500
+    end
   end
 
   def show
@@ -13,12 +18,13 @@ class RequestController < ApplicationController
 
   def get_request(mondo_id, transaction_id)
     begin
-      request = Request.find(:conditions=>["mondo_id=? and transaction_id=?",
-                                            mondo_id, transaction_id])
+      requests = Request.where "mondo_id=\'#{mondo_id}\' \
+                                AND transaction_id=\'#{transaction_id}\'"
     rescue
-      render :file => "#{Rails.root}/public/404.html",  :status => 404
+      # render :file => "#{Rails.root}/public/404.html",  :status => 404
+      render :text => "#{mondo_id} - #{transaction_id}",  :status => 404
     end
 
-    return request
+    return requests[0]
   end
 end
